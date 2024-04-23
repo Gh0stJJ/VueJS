fa4aff0bdb2d60eb021b448483a4ae85db926ed8
Juan Valdiviezo <valdiviezojuan7@gmail.com>
2024-04-22 20:41:02 -0500

    Add structure

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,47 +1,103 @@
+<template>
+  <nav id="navbar">
+    <img src="./assets/logo.svg"  width="50"/>
+    <div id="brand">
+      Todo List App
+    </div>
+
+  </nav>
+
+  <main class="container">
+    <section>
+      <form class="add-todo-form">
+        <input type="text" placeholder="Todo Title" />
+        <div>
+          <button type="submit">Add Todo</button>
+        </div>
+      </form>
+    </section>
+    <section>
+      <div id="todo">
+        <p>Pasear al perro</p>
+        <div>
+          <!-- Caracter de x para cerrar -->
+          <button id="remove-todo-btn">&times;</button> 
+        </div>
+      </div>
+    </section>
+  </main>
+  
+ 
+</template>
+
+
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
 <!-- Add scoped to limit the styles to this component -->
 <style scoped> 
-header {
-  line-height: 1.5;
+#navbar{
+  display: flex;
+  
+  align-items: center;
+  background: var(--navbar-color);
+  padding: 20px;
+  margin-bottom: 30px;
+  
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#brand{
+  font-size: 1.5rem;
+  margin-left: 20px;
+  color: var(--text-color);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.add-todo-form{
+  display: flex;
+  /* Uno a la izquierda y otro a la derecha */
+  justify-content: space-between; 
+  margin-top: 20px;
 }
+
+.add-todo-form input{
+  padding: 10px;
+  width: 80%;
+  font-size: 1rem;
+  border: solid 2px var(--border-color);
+}
+
+.add-todo-form button{
+  padding: 10px 20px;
+  font-size: 1rem;
+  background: var(--border-color);
+  color: var(--text-color);
+  border: none;
+  cursor: pointer;
+  height: 50px;
+}
+
+#todo{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--border-color);
+  margin-top: 30px;
+  padding: 0 20px 0 20px;
+  border-radius: 10px;
+}
+
+#remove-todo-btn{
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
+  font-size: 30px;
+  background: var(--remove-btn-color);
+  color: var(--text-color);
+}
+
 </style>
